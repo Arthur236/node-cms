b877314dbd4ac0c652f3a469199da577c60da329
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-01-03 14:15:18 +0300

    Deleting posts

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,8 +1,29 @@
-const elements = document.querySelectorAll('.editable');
-
-const editor = new MediumEditor(elements, {
+const editor = new MediumEditor('.editable', {
+  toolbar: {
+    buttons: [
+      'bold',
+      'italic',
+      'underline',
+      'anchor',
+      'h2',
+      'h3',
+      'quote',
+      'removeFormat',
+      'html'
+    ]
+  },
   placeholder: {
     text: 'Type your description',
     hideOnClick: false
-  }
+  },
+  forcePlainText: false,
+  autoLink: true
+});
+
+const nonEditableEditor = new MediumEditor('.editor', {
+  disableEditing: true,
+  toolbar: false,
+  placeholder: false,
+  forcePlainText: false,
+  autoLink: true
 });
