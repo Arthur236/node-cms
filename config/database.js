81b949c9dab9a86b673f589ec909b6604c8cf8fe
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-02-15 18:27:46 +0300

    Add deployment configs

--- config/database.js
+++ config/database.js
@@ -1,3 +1,11 @@
+let mongoDBUrl = '';
+
+if (process.env.NODE_ENV === 'development') {
+  mongoDBUrl = process.env.DEV_DATABASE_URL;
+} else if (process.env.NODE_ENV === 'production') {
+  mongoDBUrl = process.env.PROD_DATABASE_URL;
+}
+
 module.exports = {
-  mongoDBUrl: 'mongodb://localhost:27017/node-cms',
+  mongoDBUrl
 };
